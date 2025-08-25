dba3f4c1acd1f9751117f9f967d6a2f254c8f62b
NICOLAShsdo <164954695+NICOLAShsdo@users.noreply.github.com>
2025-08-25 20:24:58 -0300

    Add files via upload

--- App/screens/Um.js
+++ App/screens/Um.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import {View,StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
 export default function Um() {
-    return (
-        <View style={styles.container}>
-            <View style={style.box1} />
-            <View style={styles.box2}/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      {/* Parte de cima dividida em 2 colunas */}
+      <View style={styles.top}>
+        <View style={styles.left} />
+        <View style={styles.right} />
+      </View>
+
+      {/* Parte de baixo */}
+      <View style={styles.bottom} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        flexDirection: 'column',
-        paddingTop: Constants.statusBarHeight,
-    },
-box1: {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: Constants.statusBarHeight,
+  },
+  top: {
     flex: 0.5,
-    backgroundColor: 'crimson'
-},
-box2: {
+    flexDirection: 'row', // filhos em linha (lado a lado)
+  },
+  left: {
+    flex: 0.5,
+    backgroundColor: 'lime',
+  },
+  right: {
+    flex: 0.5,
+    backgroundColor: 'aquamarine',
+  },
+  bottom: {
     flex: 0.5,
     backgroundColor: 'salmon',
-},
-
+  },
 });
